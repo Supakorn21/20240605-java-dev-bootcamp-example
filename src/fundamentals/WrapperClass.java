5682539d490977e6b42504f22fefbdaadea9497b
Supakorn Charoen <supakorncharoen@gmail.com>
2026-08-10 20:13:07 +0700

    WrapperClass

---
+++ src/fundamentals/WrapperClass.java
@@ -0,0 +1,51 @@
+
+package fundamentals;
+
+import java.math.BigDecimal;
+
+public class WrapperClass {
+    public static void main(String[] args){
+        basicWrapper();
+        parseTechnique();
+    }
+
+
+    public static void basicWrapper(){
+        byte exampleOfByte = 127;
+
+        Byte exampleOfByteBoxing =  Byte.valueOf(exampleOfByte); //Boxing
+
+        System.out.println(exampleOfByteBoxing.byteValue());// Unboxing
+        // Example Of auto Boxing
+        Byte autoBoxing = exampleOfByte;
+        // Example of auto unboxing
+        System.out.println(exampleOfByteBoxing);
+
+        Short shortData = 100;
+        Integer integerData = 100;
+        Long longData = 100L;
+
+        Double doubleData = 25.00;
+        Float floatData = 25.00f;
+
+        Character grade = 'D';
+        System.out.println(STR."isLetterOrDigit: \{Character.isLetterOrDigit(grade)}");
+
+
+//        boolean isEnabled = true;
+        Boolean isEnabled = true;
+
+    }
+
+    public static void parseTechnique(){
+        //Auto unboxing -> Integer (wrapper) => int (primitive)
+        int operand1 = Integer.parseInt("500");
+        int operand2 = Integer.parseInt("100");
+
+        System.out.println(STR."Add : \{operand1 + operand2}");
+
+        Boolean.parseBoolean("true");
+
+
+    }
+}
